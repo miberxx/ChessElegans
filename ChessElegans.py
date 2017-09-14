697eb9d2d0fdec6fb666c966ee27f4a24623531a
Michael <miberxx@gmail.com>
2017-09-14 08:00:54 -0500

    nqa

--- ChessElegans.py
+++ ChessElegans.py
@@ -1,21 +1,19 @@
-import os
-import datetime
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Dropout
 from keras.callbacks import ModelCheckpoint
 import numpy as np
 import matplotlib.pyplot as plt
 #==============================================================================================================================================================
 class Params:
-    CELEGANS_INPUT_FILE = 'C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\4_out.txt'
+    CELEGANS_INPUT_FILE = 'C:\\Users\\Michael\\Desktop\\ChessElegans\\training_out.txt'
     TRAIN_PERCENTAGE = 0.9
     VALIDATION_SPLIT = 0.1
     CHECKPOINT_MODEL = True
-    CHECKPOINT_FILE_WEIGHTS = 'C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\ModelRun\\weights.best.hdf5'
-    CHECKPOINT_FILE_MODEL = 'C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\ModelRun\\model.json'
+    CHECKPOINT_FILE_WEIGHTS = 'C:\\Users\\Michael\\Desktop\\ChessElegans\\ModelRun\\weights.best.hdf5'
+    CHECKPOINT_FILE_MODEL = 'C:\\Users\\Michael\\Desktop\\ChessElegans\\ModelRun\\model.json'
     PLOT_MODEL = True
     BATCH_SIZE = 10
-    EPOCHS = 5
+    EPOCHS = 200
 #==============================================================================================================================================================
 def read_input_file():
     dataX = []
@@ -52,21 +50,30 @@ def plot_model(model_history):
 
 #==============================================================================================================================================================
 np.random.seed(0)
-
+print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
+print('Reading file...')
 trainX, testX, trainY, testY = read_input_file()
-
+print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
+print('Creating model...')
 model = Sequential()
 model.add(Dense(4400, input_dim = len(trainX[0]), init = 'normal', activation = 'relu'))
+model.add(Dropout(0.4))
 model.add(Dense(2200, init = 'normal', activation = 'relu'))
+model.add(Dropout(0.4))
 model.add(Dense(1100, init = 'normal', activation = 'relu'))
+model.add(Dropout(0.4))
 model.add(Dense(len(trainY[0]),activation = 'sigmoid'))
+print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
+print('Compiling model...')
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 if Params.CHECKPOINT_MODEL:
-    checkpoint = ModelCheckpoint(Params.CHECKPOINT_FILE_WEIGHTS, monitor='val_acc', verbose=0, save_best_only=True, mode='max')
+    checkpoint = ModelCheckpoint(Params.CHECKPOINT_FILE_WEIGHTS, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     callbacks_list = [checkpoint]
-    history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size = Params.BATCH_SIZE, epochs = Params.EPOCHS, callbacks=callbacks_list, verbose=2)
-    score = model.evaluate(testX, testY, verbose = 0)
+    history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size = Params.BATCH_SIZE, epochs = Params.EPOCHS, callbacks=callbacks_list, verbose=1)
+    score = model.evaluate(testX, testY, verbose = 1)
+    print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
     print('Model evaluation on testY, accuracy :' + "%.2f%%" % (score[1] * 100))
+    print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
     print("Serializing model to JSON...")
     model_json = model.to_json()
     with open(Params.CHECKPOINT_FILE_MODEL, 'w') as json_file:
