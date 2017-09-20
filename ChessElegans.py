02027be6bb526e85eee106b5b4ce6b57af7f74fd
mbergbauer1 <mbergbauer1@gmail.com>
2017-09-19 22:03:08 -0500

    na

--- ChessElegans.py
+++ ChessElegans.py
@@ -4,17 +4,19 @@ from keras.callbacks import ModelCheckpoint
 import numpy as np
 import matplotlib.pyplot as plt
 import timeit
+from keras.models import model_from_json
 #==============================================================================================================================================================
 class Params:
-    CELEGANS_INPUT_FILE = 'C:\\Users\\michael\\Desktop\\ChessElegans\\10K_out.txt'
+    CELEGANS_INPUT_FILE = 'C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\1K_out.txt'
     TRAIN_PERCENTAGE = 0.9
     VALIDATION_SPLIT = 0.1
     CHECKPOINT_MODEL = True
-    CHECKPOINT_FILE_WEIGHTS = 'C:\\Users\\michael\\Desktop\\ChessElegans\\ModelRun\\weights.best.hdf5'
-    CHECKPOINT_FILE_MODEL = 'C:\\Users\\michael\\Desktop\\ChessElegans\\ModelRun\\model.json'
+    CHECKPOINT_FILE_WEIGHTS = 'C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\ModelRun\\weights.best.hdf5'
+    CHECKPOINT_FILE_MODEL = 'C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\ModelRun\\model.json'
+    LOAD_MODEL = False
     PLOT_MODEL = True
     BATCH_SIZE = 10
-    EPOCHS = 100
+    EPOCHS = 10
 #==============================================================================================================================================================
 def read_input_file():
     dataX = []
@@ -47,44 +49,91 @@ def plot_model(model_history):
     plt.legend(['train','test','loss'], loc = 'upper left')
     plt.show()
 #==============================================================================================================================================================
+def load_model():
+    json_file = open(Params.CHECKPOINT_FILE_MODEL, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights(Params.CHECKPOINT_FILE_WEIGHTS)
+    return loaded_model
+#==============================================================================================================================================================
+
+
 np.random.seed(0)
 print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
 print('Reading file...')
 trainX, testX, trainY, testY = read_input_file()
 print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
-print('Creating model...')
-model = Sequential()
-model.add(Dense(600, input_dim = len(trainX[0]), init = 'RandomNormal', activation = 'relu'))
-model.add(Dense(400, init = 'RandomNormal', activation = 'relu'))
-model.add(Dense(200, init = 'RandomNormal', activation = 'relu'))
-model.add(Dense(100, init = 'RandomNormal', activation = 'relu'))
-#model.add(Dense(100, init = 'normal', activation = 'sigmoid'))
-model.add(Dense(len(trainY[0]),activation = 'sigmoid'))
-print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
-print('Compiling model...')
-model.compile(optimizer = 'adam', loss = 'mse', metrics = ['acc'])
-if Params.CHECKPOINT_MODEL:
-    print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
-    print("Serializing model to JSON...")
-    model_json = model.to_json()
-    with open(Params.CHECKPOINT_FILE_MODEL, 'w') as json_file:
-        json_file.write(model_json)
-    model.summary()
-    checkpoint = ModelCheckpoint(Params.CHECKPOINT_FILE_WEIGHTS, monitor='acc', verbose=1, save_best_only=True, mode='max')
-    callbacks_list = [checkpoint]
-    start = timeit.default_timer()
-    history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size = Params.BATCH_SIZE, epochs = Params.EPOCHS, callbacks=callbacks_list, verbose=1)
-    stop = timeit.default_timer()
-    score = model.evaluate(testX, testY, verbose = 0)
-    print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
-    print('Model evaluation on testY, accuracy :' + "%.2f%%" % (score[1] * 100))
-    print('Time to train model: '+ str(stop-start))
-    if Params.PLOT_MODEL:
-        plot_model(history)
+if Params.LOAD_MODEL:
+    print('Loading model and weights...')
+    model = load_model()
+    print('Compiling model...')
+    model.compile(optimizer='adam', loss='mse', metrics=['acc'])
+    if Params.CHECKPOINT_MODEL:
+        print(
+            '--------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print("Serializing model to JSON...")
+        model_json = model.to_json()
+        with open(Params.CHECKPOINT_FILE_MODEL, 'w') as json_file:
+            json_file.write(model_json)
+        model.summary()
+        checkpoint = ModelCheckpoint(Params.CHECKPOINT_FILE_WEIGHTS, monitor='acc', verbose=1, save_best_only=True,
+                                     mode='max')
+        callbacks_list = [checkpoint]
+        start = timeit.default_timer()
+        history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size=Params.BATCH_SIZE,
+                            epochs=Params.EPOCHS, callbacks=callbacks_list, verbose=1)
+        stop = timeit.default_timer()
+        score = model.evaluate(testX, testY, verbose=0)
+        print(
+            '--------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print('Model evaluation on testY, accuracy :' + "%.2f%%" % (score[1] * 100))
+        print('Time to train model: ' + str(stop - start))
+        if Params.PLOT_MODEL:
+            plot_model(history)
+    else:
+        history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size=Params.BATCH_SIZE,
+                            epochs=Params.EPOCHS, verbose=1)
+        score = model.evaluate(testX, testY, verbose=0)
+        print("%s: %.2f%%" % (model.metrics_names[1], score[1] * 100))
+        score = model.evaluate(testX, testY, verbose=0)
+        if Params.PLOT_MODEL:
+            plot_model(history)
+
 else:
-    history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size=Params.BATCH_SIZE, epochs=Params.EPOCHS, verbose=1)
-    score = model.evaluate(testX, testY, verbose=0)
-    print("%s: %.2f%%" % (model.metrics_names[1], score[1] * 100))
-    score = model.evaluate(testX, testY, verbose=0)
-    if Params.PLOT_MODEL:
-        plot_model(history)
+    print('Creating model...')
+    model = Sequential()
+    model.add(Dense(600, input_dim = len(trainX[0]), init = 'RandomNormal', activation = 'relu'))
+    model.add(Dense(400, init = 'RandomNormal', activation = 'relu'))
+    model.add(Dense(200, init = 'RandomNormal', activation = 'relu'))
+    model.add(Dense(100, init = 'RandomNormal', activation = 'relu'))
+    #model.add(Dense(100, init = 'normal', activation = 'sigmoid'))
+    model.add(Dense(len(trainY[0]),activation = 'sigmoid'))
+    print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
+    print('Compiling model...')
+    model.compile(optimizer = 'adam', loss = 'mse', metrics = ['acc'])
+    if Params.CHECKPOINT_MODEL:
+        print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print("Serializing model to JSON...")
+        model_json = model.to_json()
+        with open(Params.CHECKPOINT_FILE_MODEL, 'w') as json_file:
+            json_file.write(model_json)
+        model.summary()
+        checkpoint = ModelCheckpoint(Params.CHECKPOINT_FILE_WEIGHTS, monitor='acc', verbose=1, save_best_only=True, mode='max')
+        callbacks_list = [checkpoint]
+        start = timeit.default_timer()
+        history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size = Params.BATCH_SIZE, epochs = Params.EPOCHS, callbacks=callbacks_list, verbose=1)
+        stop = timeit.default_timer()
+        score = model.evaluate(testX, testY, verbose = 0)
+        print('--------------------------------------------------------------------------------------------------------------------------------------------------------')
+        print('Model evaluation on testY, accuracy :' + "%.2f%%" % (score[1] * 100))
+        print('Time to train model: '+ str(stop-start))
+        if Params.PLOT_MODEL:
+            plot_model(history)
+    else:
+        history = model.fit(trainX, trainY, validation_split=Params.VALIDATION_SPLIT, batch_size=Params.BATCH_SIZE, epochs=Params.EPOCHS, verbose=1)
+        score = model.evaluate(testX, testY, verbose=0)
+        print("%s: %.2f%%" % (model.metrics_names[1], score[1] * 100))
+        score = model.evaluate(testX, testY, verbose=0)
+        if Params.PLOT_MODEL:
+            plot_model(history)
--- Split_Input.py
+++ Split_Input.py
@@ -1,32 +1,57 @@
 import chess.pgn
 
-RAW_INPUT_FILE = "C:\\Users\\michael\\Desktop\\ChessElegans\\HQ.pgn"
-OUTPUT_FILE = "C:\\Users\\michael\\Desktop\\ChessElegans\\10K.pgn"
-SPLIT_NO_GAMES = 10000
-
+RAW_INPUT_FILE = "C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\HQ.pgn"
+OUTPUT_FILE_PATH = "C:\\Users\\mbergbauer\\Desktop\\ChessElegans\\"
+SPLIT_START = 2
+SPLIT_END = 4
+SPLIT_CONSECUTIVE = False
+SPLIT_INTERVAL = 100
+SPLIT_TOTAL_GAMES = 1000
+#==============================================================================================================================================================
 def split_CElegans_Input_File(inpath, intxtenc, outpath, outtxtenc):
     print('Opening file for read ' + inpath + ' with encoding ' + intxtenc)
     pgn_input_file_handle = open(inpath, encoding = intxtenc)
-    print('opeing file for write ' + outpath + ' with encoding ' + outtxtenc)
-    pgn_output_file_handle = open(outpath, 'w', encoding = outtxtenc)
+    file_name = str(SPLIT_START) + '_' + str(SPLIT_END) + '.pgn'
+    out_file = outpath + file_name
+    print('opeing file for write ' + out_file + ' with encoding ' + outtxtenc)
+    pgn_output_file_handle = open(out_file, 'w', encoding = outtxtenc)
 
-    count_games = 1
+    count_games = 0
     print('----------------------------------------------------------------------------------------------------------------------------------------------------')
     print('Reading games...')
-    while count_games <= SPLIT_NO_GAMES:
+    while count_games <= SPLIT_END:
         game = chess.pgn.read_game(pgn_input_file_handle)
         if game == None:
             break
         if game.errors == []:
             count_games += 1
             #pgn_output_file_handle.write(game)
-            print(game, file=pgn_output_file_handle , end="\n\n")
-            print(str(SPLIT_NO_GAMES) + '/' + str(count_games-1))
+            if count_games >= SPLIT_START and count_games <= SPLIT_END:
+                print(game, file=pgn_output_file_handle , end="\n\n")
+                print(str(SPLIT_START)+'-'+str(SPLIT_END) + ' / ' + str(count_games))
         else:
             continue
     pgn_output_file_handle.close()
     print('----------------------------------------------------------------------------------------------------------------------------------------------------')
     print('Games read: ' + str(count_games-1))
+    print('Games written: ' + str(SPLIT_END - SPLIT_START +1))
+#==============================================================================================================================================================
+def split_consecutive(inpath, intxtenc, outpath, outtxtenc):
+    print('Opening file for read ' + inpath + ' with encoding ' + intxtenc)
+    pgn_input_file_handle = open(inpath, encoding=intxtenc)
+    no_files = int(SPLIT_TOTAL_GAMES/SPLIT_INTERVAL)+1
+
+    for n in range(0, no_files,1):
 
 
-split_CElegans_Input_File(RAW_INPUT_FILE,'ansi',OUTPUT_FILE,'utf-8')
+
+
+
+
+
+
+#==============================================================================================================================================================
+if not SPLIT_CONSECUTIVE:
+    split_CElegans_Input_File(RAW_INPUT_FILE,'ansi',OUTPUT_FILE_PATH,'utf-8')
+else:
+    split_consecutive(RAW_INPUT_FILE,'ansi',OUTPUT_FILE_PATH,'utf-8')
